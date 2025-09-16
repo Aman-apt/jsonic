226283b9f1aa86b1bc70594bf5f6d5ff1f3b7a2f
Aman-apt <amanyadav665078@gmail.com>
2025-09-16 13:09:14 +0530

    Intial Commit

---
+++ constants.py
@@ -0,0 +1,10 @@
+
+# Global constants for parsers
+
+JSON_COMMA = ','
+JSON_COLON = ':'
+JSON_LEFTBRACKET = '['
+JSON_RIGHTBRACKET = ']'
+JSON_LEFTBRACE = '{'
+JSON_RIGHTBRACE = '}'
+JSON_QUOTE = '""'
